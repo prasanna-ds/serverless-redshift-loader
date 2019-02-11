073af8fbf5b7b4b00ce2ebd519daba3f0352e06a
Prasanna Dhongudi Sudhindrakumar <prasanna.sudhindrakumar@gmail.com>
2019-02-11 23:06:18 +0800

    Update address.sql

--- sql/address/address.sql
+++ sql/address/address.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS immobilienscout24_berlin.address (
-  listing_id                                    INTEGER       ENCODE DELTA32K NOT NULL,
+  address_id                                    INTEGER       ENCODE DELTA32K NOT NULL,
   realEstate_address_city                       VARCHAR(20)   ENCODE LZO,
   realEstate_address_geoHierarchy_city_name     VARCHAR(20)   ENCODE LZO,
   realEstate_address_geoHierarchy_country_name  VARCHAR(10)   ENCODE LZO,
